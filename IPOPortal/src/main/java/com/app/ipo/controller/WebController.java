1a8ded2d5ac9de20c5a2631d09813683c764ddb1
anushkaseth532000 <anushka.seth.532000@gmail.com>
2023-08-31 17:35:07 +0530

    Final Project

---
+++ IPOPortal/src/main/java/com/app/ipo/controller/WebController.java
@@ -0,0 +1,57 @@
+package com.app.ipo.controller;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.app.ipo.dto.AdminDto;
+import com.app.ipo.dto.UserDto;
+
+@Controller
+public class WebController 
+{	
+   @RequestMapping(value = "/index")
+   public String index(@Valid UserDto userDto, BindingResult result, Model model) 
+   {
+	   ObjectError error = new ObjectError("globalError", "");
+       result.addError(error);
+       
+      return "index";
+   }
+   
+   @RequestMapping(value = "/employeehome")
+   public String employeehome(@Valid UserDto userDto, BindingResult result, Model model) 
+   {
+	   ObjectError error = new ObjectError("globalError", "");
+       result.addError(error);
+       
+      return "employeehome";
+   }
+  
+   @RequestMapping(value = "/admin")
+   public String admin(@Valid AdminDto adminDto, BindingResult result, Model model) 
+   {
+	   ObjectError error = new ObjectError("globalError", "");
+       result.addError(error);
+   
+      return "admin";
+   }
+   
+   @RequestMapping(value = "/logout")
+   public String logout(@Valid UserDto userDto, BindingResult result, Model model, HttpSession session) 
+   {
+	  ObjectError error = new ObjectError("globalError", "Logged out succesfully!");
+      result.addError(error);
+      
+      //session  close
+      session.getAttribute("x");
+         
+      //invalidate session or destroy session
+      session.invalidate();
+       
+      return "employeehome";
+   }
+}
